34e503b8bd267562ec2cced97750e524de976652
claytontaylor <claytontaylor@cox.net>
2016-07-05 15:32:17 -0600

    String-Reporter First Half

---
+++ JS-String-Reporter.js
@@ -0,0 +1,10 @@
+var word = prompt("Please Type in a Word");
+var length = word.length
+
+if (word.toLowerCase() == "verbatim") {
+  alert(word + ":" + " " + "String Length" + " " + "=" + " " + length);
+}
+else {
+	console.log("Please enter a word.")
+}
+
